6f982d0f7d4b4c7e04cca24b0f647d7940f696a1
Marton Borlay <borlaym@gmail.com>
2018-09-21 13:16:08 +0200

    feat(createMap): add createMap function based on  image

---
+++ src/createMap.ts
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+
+export default function createMap(scene: THREE.Scene, mapDefiniton: HTMLImageElement) {
+	const roomTexture = new THREE.TextureLoader().load("textures/1.jpg");
+	roomTexture.wrapS = THREE.RepeatWrapping;
+	roomTexture.wrapT = THREE.RepeatWrapping;
+	roomTexture.repeat.set(1, 1);
+
+	const tileGeometry = new THREE.CubeGeometry(1, 1, 1, 1, 1, 1);
+	const floorGeometry = new THREE.PlaneGeometry(1, 1);
+	const roomMaterial = new THREE.MeshBasicMaterial({ color: 0xaaaaaa, map: roomTexture, side: THREE.DoubleSide });
+	const floorMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+
+	// Read map definition image
+	const canvas: HTMLCanvasElement = document.createElement('canvas');
+	canvas.width = mapDefiniton.width;
+	canvas.height = mapDefiniton.height;
+	const ctx = canvas.getContext('2d');
+	if (!ctx) {
+		throw new Error('Couldnt initialize canvas context for drawing map')
+	}
+	ctx.drawImage(mapDefiniton, 0, 0);
+	const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+	const tiles: THREE.Mesh[] = [];
+	for (let i = 0; i < imageData.data.length; i += 4) {
+		const r = imageData.data[i];
+		const g = imageData.data[i + 1];
+		const b = imageData.data[i + 2];
+		const row = Math.floor((i / 4) / mapDefiniton.width);
+		const col = (i / 4) % mapDefiniton.width;
+		// On white, add a tile
+		if (r === 255 && g === 255 && b === 255) {
+			const tileMesh = new THREE.Mesh(tileGeometry, roomMaterial);
+			tileMesh.position.x = row;
+			tileMesh.position.z = col;
+			tiles.push(tileMesh)
+			scene.add(tileMesh);
+		}
+		// On gray, add a floor tile
+		if (r === 51 && g === 51 && b === 51) {
+			const tileMesh = new THREE.Mesh(floorGeometry, floorMaterial);
+			tileMesh.rotation.x = Math.PI / 2
+			tileMesh.position.x = row;
+			tileMesh.position.z = col;
+			tileMesh.position.y = -0.5;
+			tiles.push(tileMesh)
+			scene.add(tileMesh);
+		}
+	}
+
+	return tiles;
+}
+
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,102 +1,106 @@
-import createPlayerMesh from './createPlayerMesh';
-import createRoom from './createRoom';
-import * as socketio from 'socket.io-client';
+// import createPlayerMesh from './createPlayerMesh';
+import createMap from 'createMap';
+// import * as socketio from 'socket.io-client';
 import * as THREE from 'three';
 import { uniq } from 'lodash';
 
-const serverName = process.env.NODE_ENV === 'production' ? 'https://marci-fps-test.herokuapp.com' : 'http://localhost:3001';
-const connection = socketio(serverName);
-
+// const serverName = process.env.NODE_ENV === 'production' ? 'https://marci-fps-test.herokuapp.com' : 'http://localhost:3001';
+// const connection = socketio(serverName);
 const SPEED = 0.1;
-const TURN_SPEED = 4;
-
+// const TURN_SPEED = 4;
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 4;
+camera.position.y = 4;
+camera.position.z = 16;
+camera.position.x = 16;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 renderer.domElement.onclick = () => {
 	renderer.domElement.requestPointerLock();
 }
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const room = createRoom(scene);
+const mapDefinition = new Image();
+mapDefinition.onload = start;
+mapDefinition.src = 'maps/map.png';
 
-const colliders: THREE.Object3D[] = [room];
-
-let players: InterfacePlayer[] = [];
-
-interface InterfacePlayer {
-	name: string,
-	hp: number,
-	mesh: THREE.Mesh
+function start() {
+	const tiles = createMap(scene, mapDefinition);
+	camera.lookAt(tiles[5].position);
 }
 
-interface InterfaceGameState {
-	players: Array<{
-		hp: number,
-		name: string,
-		ry: number,
-		x: number,
-		y: number,
-		z: number
-	}>,
-	yourHp: number
-};
+// const colliders: THREE.Object3D[] = [map];
 
-const playerMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
-const hpdisplay = document.querySelector('.hp');
+// let players: InterfacePlayer[] = [];
 
-connection.on('gameState', (data: InterfaceGameState) => {
-	data.players.forEach(player => {
-		const localPlayer = players.find(p => p.name === player.name);
-		if (localPlayer) {
-			// Update existing player
-			localPlayer.mesh.position.set(player.x, player.y, player.z);
-			localPlayer.mesh.rotation.y = player.ry;
-			localPlayer.hp = player.hp;
-			const mat = playerMaterial.clone();
-			mat.color.setHSL(0, 1, player.hp / 100);
-			localPlayer.mesh.material = mat;
-		} else {
-			// Add new player
-			const mesh = createPlayerMesh();
-			players.push({
-				mesh,
-				hp: player.hp,
-				name: player.name
-			});
-			mesh.position.set(player.x, player.y, player.z);
-			mesh.rotation.y = player.ry;
-			const mat = playerMaterial.clone();
-			mat.color.setHSL(0, 1, player.hp / 100);
-			mesh.material = mat;
-			scene.add(mesh);
-		}
-	});
-	const removedPlayers: InterfacePlayer[] = [];
-	// Remove non-existent players
-	players.forEach(player => {
-		const remotePlayer = data.players.find(p => p.name === player.name);
-		if (!remotePlayer) {
-			scene.remove(player.mesh);
-			removedPlayers.push(player);
-		}
-	});
-	players = players.filter(player => removedPlayers.indexOf(player) === -1);
-	if (hpdisplay) {
-		hpdisplay.innerHTML = String(data.yourHp);
-	}
-});
+// interface InterfacePlayer {
+// 	name: string,
+// 	hp: number,
+// 	mesh: THREE.Mesh
+// }
 
-connection.on('init', (data: { name: string }) => {
-	const div = document.createElement('div');
-	div.innerHTML = `Name: ${data.name}<br>Server: ${serverName}`;
-	div.className = 'nametag';
-	document.body.appendChild(div);
-});
+// interface InterfaceGameState {
+// 	players: Array<{
+// 		hp: number,
+// 		name: string,
+// 		ry: number,
+// 		x: number,
+// 		y: number,
+// 		z: number
+// 	}>,
+// 	yourHp: number
+// };
+
+// Wconnection.on('gameState', (data: InterfaceGameState) => {
+// 	data.players.forEach(player => {
+// 		const localPlayer = players.find(p => p.name === player.name);
+// 		if (localPlayer) {
+// 			// Update existing player
+// 			localPlayer.mesh.position.set(player.x, player.y, player.z);
+// 			localPlayer.mesh.rotation.y = player.ry;
+// 			localPlayer.hp = player.hp;
+// 			const mat = playerMaterial.clone();
+// 			mat.color.setHSL(0, 1, player.hp / 100);
+// 			localPlayer.mesh.material = mat;
+// 		} else {
+// 			// Add new player
+// 			const mesh = createPlayerMesh();
+// 			players.push({
+// 				mesh,
+// 				hp: player.hp,
+// 				name: player.name
+// 			});
+// 			mesh.position.set(player.x, player.y, player.z);
+// 			mesh.rotation.y = player.ry;
+// 			const mat = playerMaterial.clone();
+// 			mat.color.setHSL(0, 1, player.hp / 100);
+// 			mesh.material = mat;
+// 			scene.add(mesh);
+// 		}
+// 	});
+// 	const removedPlayers: InterfacePlayer[] = [];
+// 	// Remove non-existent players
+// 	players.forEach(player => {
+// 		const remotePlayer = data.players.find(p => p.name === player.name);
+// 		if (!remotePlayer) {
+// 			scene.remove(player.mesh);
+// 			removedPlayers.push(player);
+// 		}
+// 	});
+// 	players = players.filter(player => removedPlayers.indexOf(player) === -1);
+// 	if (hpdisplay) {
+// 		hpdisplay.innerHTML = String(data.yourHp);
+// 	}
+// });
+
+// connection.on('init', (data: { name: string }) => {
+// 	const div = document.createElement('div');
+// 	div.innerHTML = `Name: ${data.name}<br>Server: ${serverName}`;
+// 	div.className = 'nametag';
+// 	document.body.appendChild(div);
+// });
 
 interface InterfaceState {
 	keysDown: string[],
@@ -122,19 +126,19 @@ document.addEventListener('keyup', (event) => {
 	state.keysDown = state.keysDown.filter(key => key !== event.key);
 });
 
-document.addEventListener('click', () => {
-	const raycaster = new THREE.Raycaster();
-	raycaster.setFromCamera(new THREE.Vector2(), camera);
-	const results = raycaster.intersectObjects(players.map(player => player.mesh));
-	if (results.length) {
-		const targetPlayer = players.find(p => p.mesh === results[0].object);
-		if (targetPlayer) {
-			connection.emit('hit', {
-				name: targetPlayer.name
-			});
-		}
-	}
-});
+// document.addEventListener('click', () => {
+// 	const raycaster = new THREE.Raycaster();
+// 	raycaster.setFromCamera(new THREE.Vector2(), camera);
+// 	const results = raycaster.intersectObjects(players.map(player => player.mesh));
+// 	if (results.length) {
+// 		const targetPlayer = players.find(p => p.mesh === results[0].object);
+// 		if (targetPlayer) {
+// 			connection.emit('hit', {
+// 				name: targetPlayer.name
+// 			});
+// 		}
+// 	}
+// });
 
 const onMouseMove = (event: MouseEvent) => {
 	state.mouseMovement.x += event.movementX;
@@ -153,16 +157,16 @@ const lockChangeAlert = () => {
 document.addEventListener('pointerlockchange', lockChangeAlert, false);
 
 function animate() {
-	if (state.mouseMovement.x || state.mouseMovement.y) {
-		const dx = state.mouseMovement.x || 0;
-		camera.rotation.y -= dx / window.innerWidth * TURN_SPEED;
+	// if (state.mouseMovement.x || state.mouseMovement.y) {
+	// 	const dx = state.mouseMovement.x || 0;
+	// 	camera.rotation.y -= dx / window.innerWidth * TURN_SPEED;
 
-		const dy = state.mouseMovement.y || 0;
-		camera.rotation.x -= dy / window.innerHeight * TURN_SPEED;
-		camera.rotation.x = Math.min(camera.rotation.x, 0.75);
-		camera.rotation.x = Math.max(camera.rotation.x, -0.75);
-		camera.rotation.order = 'YXZ'
-	}
+	// 	const dy = state.mouseMovement.y || 0;
+	// 	camera.rotation.x -= dy / window.innerHeight * TURN_SPEED;
+	// 	camera.rotation.x = Math.min(camera.rotation.x, 0.75);
+	// 	camera.rotation.x = Math.max(camera.rotation.x, -0.75);
+	// 	camera.rotation.order = 'YXZ'
+	// }
 	const motion = new THREE.Vector3(0, 0, 0);
 	if (state.keysDown.indexOf('w') > -1) {
 		motion.z -= SPEED;
@@ -176,24 +180,24 @@ function animate() {
 	if (state.keysDown.indexOf('d') > -1) {
 		motion.x += SPEED;
 	}
-	motion.applyEuler(new THREE.Euler(0, camera.rotation.y, 0));
+	// motion.applyEuler(new THREE.Euler(0, camera.rotation.y, 0));
 
-	const raycaster = new THREE.Raycaster(camera.position, motion.clone().normalize());
-	const results = raycaster.intersectObjects(colliders);
-	const collisions = results.filter(result => result.distance < 1);
-	if (!collisions.length) {
+	// const raycaster = new THREE.Raycaster(camera.position, motion.clone().normalize());
+	// const results = raycaster.intersectObjects(colliders);
+	// const collisions = results.filter(result => result.distance < 1);
+	// if (!collisions.length) {
 		camera.position.add(motion);
-	}
+	// }
 
 	state.mouseMovement.x = 0;
 	state.mouseMovement.y = 0;
 
-	connection.emit('state', {
-		ry: camera.rotation.y,
-		x: camera.position.x,
-		y: -1,
-		z: camera.position.z
-	});
+	// connection.emit('state', {
+	// 	ry: camera.rotation.y,
+	// 	x: camera.position.x,
+	// 	y: -1,
+	// 	z: camera.position.z
+	// });
 
 	requestAnimationFrame(animate);
 	renderer.render(scene, camera);
